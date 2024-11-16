e6aaf63345ba2769a2f9916c4e8f1885f188e57e
Евгений Москалянов <emos@inbox.ru>
2024-11-17 00:33:47 +0300

    Включена российская локаль в настройках.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,9 +108,9 @@ AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
