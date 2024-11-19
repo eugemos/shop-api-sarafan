8a71ecbd6a864acd08fe1e797f488a5b7e4a64c0
Евгений Москалянов <emos@inbox.ru>
2024-11-19 21:23:23 +0300

    Подключен пакет djoser.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 )
@@ -9,6 +9,11 @@ from .views import CategoryListView, ProductListView
 urlpatterns = [
     path('categories/', CategoryListView.as_view()),
     path('products/', ProductListView.as_view()),
+
+    path('auth/', include('djoser.urls')),
+    # re_path(r'^auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    # re_path(r'^auth/', include('djoser.urls.authtoken')),
 
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'drf_spectacular',
     'users',
     'categories',
@@ -136,6 +138,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
